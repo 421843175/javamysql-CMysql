c89cc20ae20491f3d43d1a34f4b9b364a5badb5b
木星 <95159189+421843175@users.noreply.github.com>
2022-07-06 16:07:08 +0800

    Add files via upload

---
+++ CMysql/src/jupiter/sql/CMysql.java
@@ -0,0 +1,128 @@
+package jupiter.sql;
+
+import java.sql.*;
+import java.util.*;
+
+public class CMysql {
+    private Connection con=null;
+
+
+    private Statement st=null;
+   private  ResultSet rs=null;
+   private ResultSetMetaData rsmd=null;
+    private int colum;
+    private LinkedHashMap<String, LinkedList<String>> lh=null;
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
+
+    public Statement getSt() {
+        return st;
+    }
+
+    public ResultSet getRs() {
+        return rs;
+    }
+
+    public ResultSetMetaData getRsmd() {
+        return rsmd;
+    }
+
+    public int getColum() {
+        return colum;
+    }
+
+    public CMysql(String adress, String port, String username, String password, String database) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        con=DriverManager.getConnection("jdbc:mysql://"+adress+":"+port+"/"+database+"?serverTimezone=GMT",username,password);
+    }
+    public CMysql(String adress,String port,String username,String password,String database,String time) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        con=DriverManager.getConnection("jdbc:mysql://"+adress+":"+port+"/"+database+"?serverTimezone="+time,username,password);
+    }
+
+    public LinkedHashMap<String, LinkedList<String>> select(String sql) throws SQLException {
+        LinkedHashMap<String, LinkedList<String>>  lh=new LinkedHashMap();
+
+        st=con.createStatement();
+        rs=st.executeQuery(sql);
+        rsmd = rs.getMetaData();
+
+        colum= rsmd.getColumnCount();
+        LinkedList<String> ll=new LinkedList<>();
+        rs=st.executeQuery(sql);
+        for(int i=1;i<=colum;i++) {
+            for (int j = 1; rs.next(); j++) {
+                ll.add(rs.getString(i));
+                lh.put(rsmd.getColumnName(i),ll);
+            }
+            ll=new LinkedList<>();
+            rs=st.executeQuery(sql);
+        }
+        this.lh=lh;
+        return lh;
+
+        }
+
+    /**
+     *
+     * @param lh
+     * @param row:多少行
+     * @param col:多少列
+     * @return
+     */
+        public String selectOne(LinkedHashMap<String, LinkedList<String>> lh,String row,int col)
+        {
+         Set bl=lh.entrySet();
+            LinkedList<String> s = lh.get(row);
+            return s.get(col);
+        }
+
+    /**
+     *插入修改删除
+     * @param  sql：语句
+     * @return 插入成功数据的行数
+     * @throws SQLException
+     */
+        public int upInDe(String sql) throws SQLException {
+            Statement st=con.createStatement();
+            int result=st.executeUpdate(sql);
+            return result;
+        }
+
+        public void selectAll(String sql) throws SQLException {
+            rs=st.executeQuery(sql);
+            for(int i=1;i<=colum;i++)
+                System.out.printf("%15s",rsmd.getColumnName(i));
+            System.out.println();
+            while(rs.next())
+            {
+                for(int i=1;i<=colum;i++)
+                    System.out.printf("%15s",rs.getString(i));
+                System.out.println();
+            }
+        }
+
+        public int isExist(String head,String str){
+            LinkedList<String> s = lh.get(head);
+            if(s==null)return -1;
+            int i = s.indexOf(str);
+            return i;
+
+        }
+
+        public LinkedHashMap<String,String> existRow(int i) throws SQLException {
+            LinkedHashMap<String,String> als=new LinkedHashMap<String,String>();
+            for(int j=1;j<=colum;j++) {
+                LinkedList<String> li = lh.get(rsmd.getColumnName(j));
+                als.put(rsmd.getColumnName(j),li.get(i));
+            }
+            return als;
+        }
+
+    }
